25d452a0620a36dc15766ee017b791e5e77eb491
Jascha030 <contact@jaschavanaalst.nl>
2022-02-23 02:04:08 +0100

    ✨ Add php version retrieval logic

--- src/LocalWPService.php
+++ src/LocalWPService.php
@@ -4,33 +4,116 @@ declare(strict_types=1);
 
 namespace Jascha030\Localphp;
 
+use InvalidArgumentException;
+use Jascha030\Localphp\Process\Binary\PhpBinary;
+use RuntimeException;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
-class LocalPhpService
+final class LocalWPService
 {
     private string $servicePath;
 
-    public function __construct(
-        private string $applicationPath,
-        private $packageDirectories = []
-    ) {
-        $this->servicePath = $this->resolveLightningServicesPath();
-    }
+    private ?array $packageDirectories;
 
-    private function resolveLightningServicesPath(): string
+    private ?array $phpVersions;
+
+    public function __construct(private string $applicationPath, private OutputInterface $output)
     {
-        $path = sprintf('%s/Contents/Resource/extraResources/lightning-services', $this->applicationPath);
-
-        return is_dir($path)
-            ? $path
-            : throw new \InvalidArgumentException(
-                // todo: make Exception subclass
-                "Could not find the lightning-services directory in: \"{$this->applicationPath}\"."
-            );
+        $this->resolveLightningServicesPath();
+        $this->phpVersions = [];
     }
 
-//    private function collection(): array
-//    {
-//        foreach ((new Finder())->in($this->servicePath)->directories()->getIterator() as $dir) {}
-//    }
+    /**
+     * @param bool $scan enable to re-scan package dir for changes between calls
+     *
+     * @throws RuntimeException
+     */
+    public function getAvailablePhpVersions(bool $scan = false): array
+    {
+        foreach ($this->getPackages($scan) as $name => $packagePath) {
+            if (str_starts_with($name, 'php')) {
+                $path = $this->findBinary($packagePath);
+
+                if (! $path) {
+                    continue;
+                }
+
+                $binary = new PhpBinary($path, $this->output);
+
+                if (! isset($this->phpVersions[$binary->getVersion()])) {
+                    $this->phpVersions[$binary->getVersion()] = $binary;
+                }
+            }
+        }
+
+        return $this->phpVersions;
+    }
+
+    /**
+     * @param bool $scan enable to re-scan package dir for changes between calls
+     */
+    public function getPackages(bool $scan = false): array
+    {
+        return $scan
+            ? $this->fetchPackages()->packageDirectories
+            : $this->packageDirectories ?? $this->fetchPackages()->packageDirectories;
+    }
+
+    /**
+     * @throws InvalidArgumentException
+     * @todo: make Exception subclass
+     */
+    private function resolveLightningServicesPath(): void
+    {
+        $path = sprintf('%s/Contents/Resources/extraResources/lightning-services', $this->applicationPath);
+
+        if (! is_dir($path)) {
+            throw new InvalidArgumentException("Could not find the lightning-services directory in: \"{$this->applicationPath}\".");
+        }
+
+        $this->servicePath = $path;
+    }
+
+    private function fetchPackages(): self
+    {
+        if (! isset($this->packageDirectories)) {
+            $this->packageDirectories = [];
+        }
+
+        $dirs = (new Finder())
+            ->in($this->servicePath)
+            ->directories()
+            ->depth(0)
+            ->getIterator();
+
+        /** @var SplFileInfo $dir */
+        foreach ($dirs as $dir) {
+            if (! isset($this->packageDirectories[$dir->getFilename()])) {
+                $this->packageDirectories[$dir->getFilename()] = $dir->getRealPath();
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @throws RuntimeException
+     */
+    private function findBinary(string $path): ?string
+    {
+        $finder = (new Finder())->in("{$path}/bin")->files()->name('php');
+
+        if ($finder->count() > 1) {
+            throw new RuntimeException('Multiple \'php\' binaries found');
+        }
+
+        $array    = iterator_to_array($finder->getIterator());
+        $fileInfo = reset($array);
+
+        return $fileInfo instanceof SplFileInfo
+            ? $fileInfo->getRealPath()
+            : null;
+    }
 }
