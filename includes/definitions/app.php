24ea5c9dc8062a1e174314138481cce3bfd9a57b
Jascha030 <contact@jaschavanaalst.nl>
2022-02-23 23:39:49 +0100

    🔧 Add definition files for php-di

---
+++ includes/definitions/app.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+use Jascha030\Localphp\Console\Application\Application;
+use Jascha030\Localphp\LocalWPService;
+use Symfony\Component\Console\Output\ConsoleOutput;
+use Symfony\Component\Console\Output\OutputInterface;
+use function DI\create;
+use function DI\get;
+
+/**
+ * Application definitions.
+ *
+ * @return array
+ *
+ * @see \DI\Container
+ * @see \Di\ContainerBuilder
+ *
+ * @see https://php-di.org/doc/php-definitions.html
+ */
+return [
+    'local_default_path'   => '/Applications/Local.app',
+    'app'                  => create(Application::class)->method('addCommands', get('commands')),
+    'local'                => create(LocalWPService::class)->constructor(get('local_default_path')),
+    OutputInterface::class => create(ConsoleOutput::class),
+];
