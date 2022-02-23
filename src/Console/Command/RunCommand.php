74610c39556f5b5b00bd95698603379cfafde4ee
Jascha030 <contact@jaschavanaalst.nl>
2022-02-23 22:34:49 +0100

    🏗️ Abstract code that will be common between php process related code

--- src/Console/Command/RunCommand.php
+++ src/Console/Command/RunCommand.php
@@ -5,59 +5,24 @@ declare(strict_types=1);
 namespace Jascha030\Localphp\Console\Command;
 
 use Jascha030\Localphp\LocalWPService;
-use Jascha030\Localphp\Process\Binary\PhpBinary;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-final class RunCommand extends Command
+final class RunCommand extends PhpCommandAbstract
 {
     private const COMMAND_NAME = 'run';
 
-    public function __construct(private LocalWPService $localWPService)
+    public function __construct(LocalWPService $localWPService)
     {
-        parent::__construct(self::COMMAND_NAME);
+        parent::__construct(self::COMMAND_NAME, $localWPService);
     }
 
-    public function configure(): void
+    public function do(InputInterface $input, OutputInterface $output): int
     {
-        $this->setName(self::COMMAND_NAME)
-            ->setDescription('Run commands with a specified version available to LocalWP, similar to `php -r [subcommand]`.')
-            ->addArgument('subcommand', InputArgument::OPTIONAL, 'The php command to execute.')
-            ->addOption('use', 'u', InputOption::VALUE_REQUIRED, 'The php version to use.')
-            ->addOption('list', 'l', InputOption::VALUE_NONE, 'List available versions')
-            ->addOption('silent', 's', InputOption::VALUE_NONE, 'Don\'t write command output to console.')
-            ->addOption('working-dir', 'w', InputOption::VALUE_OPTIONAL, 'Execute command in a specified directory.');
-    }
+        $php = $this->validateOptions($input, $output);
 
-    protected function execute(InputInterface $input, OutputInterface $output): int
-    {
-        if ($input->getOption('list')) {
-            return $this->list($input, $output);
-        }
-
-        return $this->do($input, $output);
-    }
-
-    private function do(InputInterface $input, OutputInterface $output): int
-    {
-        $this->localWPService->setOutput($output);
-
-        if (! $input->getOption('use')) {
-            $output->writeln('<error>Provide a version using the --use flag.</error>');
-
-            return Command::FAILURE;
-        }
-
-        $version  = $input->getOption('use');
-        $binaries = $this->localWPService->getAvailablePhpVersions();
-
-        if (! isset($binaries[$version])) {
-            $output->writeln('<error>No available versions found</error>');
-
-            return Command::FAILURE;
+        if (is_int($php)) {
+            return $php;
         }
 
         $subcommand = $input->getArgument('subcommand');
@@ -66,24 +31,10 @@ final class RunCommand extends Command
             $subcommand = '-v';
         }
 
-        /** @var PhpBinary $php */
-        $php = $binaries[$version];
-
         return $php(
             $php::sanitizeForRunFlag($subcommand),
             $input->getOption('working-dir'),
             $input->getOption('silent') ?? false
         );
-    }
-
-    private function list(InputInterface $input, OutputInterface $output): int
-    {
-        $versions = $this->localWPService->getAvailablePhpVersions();
-
-        foreach ($versions as $version => $binary) {
-            $output->writeln("* {$version}");
-        }
-
-        return Command::SUCCESS;
     }
 }
