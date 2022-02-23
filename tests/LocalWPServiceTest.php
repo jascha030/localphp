1053e07bd7be0a6903abca8db31ad45f0f1aaef0
Jascha030 <contact@jaschavanaalst.nl>
2022-02-23 02:06:27 +0100

    ✅ Add passing test for Local service (100% coverage for class ^^).

--- tests/LocalWPServiceTest.php
+++ tests/LocalWPServiceTest.php
@@ -4,8 +4,79 @@ namespace Jascha030\Localphp\Tests;
 
 use Jascha030\Localphp\LocalWPService;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
-class LocalWPServiceTest extends TestCase
+/**
+ * @covers \Jascha030\Localphp\LocalWPService
+ *
+ * @internal
+ */
+final class LocalWPServiceTest extends TestCase
 {
+    public function testConstruct(): LocalWPService
+    {
+        $service = new LocalWPService('/Applications/Local.app', new ConsoleOutput());
 
+        $this->assertInstanceOf(LocalWPService::class, $service);
+
+        return $service;
+    }
+
+    /**
+     * @depends testConstruct
+     */
+    public function testThrowsExceptionOnInvalidPath(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        new LocalWPService('/Applications/this/path/is/invalid', new ConsoleOutput());
+    }
+
+    /**
+     * @depends testConstruct
+     */
+    public function testGetPackages(LocalWPService $service): void
+    {
+        $this->assertIsArray($service->getPackages());
+    }
+
+    /**
+     * @depends testConstruct
+     * @depends testGetPackages
+     */
+    public function testScanAndGetPackages(LocalWPService $service): void
+    {
+        $this->assertEquals($service->getPackages(), $service->getPackages(true));
+    }
+
+    /**
+     * @depends testConstruct
+     * @depends testGetPackages
+     */
+    public function testGetAvailablePhpVersions(LocalWPService $service): void
+    {
+        $this->assertIsArray($service->getAvailablePhpVersions());
+    }
+
+    /**
+     * @depends testConstruct
+     * @depends testGetPackages
+     */
+    public function testGetAvailablePhpVersionsWithoutFoundBinaryPath(): void
+    {
+        $service = new LocalWPService(__DIR__ . '/Fixtures/no-binary-path-app/Local.app', new ConsoleOutput());
+
+        $this->assertEmpty($service->getAvailablePhpVersions());
+    }
+
+    /**
+     * @depends testConstruct
+     * @depends testGetPackages
+     */
+    public function testThrowsExceptionForMultipleBinaries(): void
+    {
+        $this->expectException(\RuntimeException::class);
+
+        (new LocalWPService(__DIR__ . '/Fixtures/double-binary-app/Local.app', new ConsoleOutput()))->getAvailablePhpVersions();
+    }
 }
